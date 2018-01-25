77e3fcad0d9c68d2e982d791e57387be2165db2c
TrashcatOrange <33126192+TrashcatOrange@users.noreply.github.com>
2018-01-25 17:14:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.write("omae wa mou shindeiru");
+
+window.onload = function(){
+
+(function(){
+  var date = new Date();
+  var time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+  document.getElementsByTagName('div')[0].innerHTML="JS CLOCK: "+time;
+  window.setTimeout(arguments.callee,1000);
+})();
+
+}
